0ae0ecb60407e61c1c356fcb5a107968f109916b
lvdelrio <lvdelrio@miuandes.cl>
2021-04-10 16:06:59 -0400

    ola wn

--- Delrio-Ballivian-Tarea1.c
+++ Delrio-Ballivian-Tarea1.c
@@ -18,7 +18,7 @@ int imprimir_pantalla(){
     char ch[100][50];
     FILE*file;
     file=fopen("posicionesJ1.txt","r");
-    printf("ola,ptoqweassas");
+    printf("ola,ptoqweassassfddsaad");
     
     //char str[MAXCHAR];
     printf("vamo vien \n");
