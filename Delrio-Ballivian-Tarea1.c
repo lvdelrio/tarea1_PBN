6235f64a1182432930a481c4487e635a38f08d9f
AgBallivian <agballivian@miuandes.cl>
2021-04-14 20:45:55 -0400

    Actualizacion 11 pero con menos  insultos al usuario

--- Delrio-Ballivian-Tarea1.c
+++ Delrio-Ballivian-Tarea1.c
@@ -23,10 +23,7 @@ int condicion_ganar(char** tablero,int player_num){
     for(int i=0;i<10;i++){
         for(int j=0;j<10;j++){
 
-            //printf("antes del if por la shit\n");
-
             if((tablero[i][j] != 'X') && (tablero[i][j] != '_') && (tablero[i][j] != '.')){
-                printf("entro esta mierda: %c\n",tablero[i][j]);
                 contador++;
                 return 0;
                 }
@@ -64,6 +61,7 @@ char** imprimir_tablero(char** matriz){
 
     return 0;
 }
+//imprimir tablero DEBUG
 char** imprimir_tablero_Cheat(char** matriz){
 
     printf("  A  B  C  D  E  F  G  H  I  J\n");
@@ -80,13 +78,8 @@ char** imprimir_tablero_Cheat(char** matriz){
 
     return 0;
 }
-
-//Duplicado para recivir info con array
-
 int traducir(char posX){
     int intposX = 0;
-
-    //printf("la wea mal hecha es: %c\n", posX);
     if (posX == 65) intposX = 0;
     else if (posX == 66) intposX = 1;
     else if (posX == 67) intposX = 2;
@@ -107,7 +100,6 @@ int comprobar(int largo, int intposX, int posY, char orientacion){
         exit(0);
     }
     if(orientacion == 104){
-        printf("%d, %c COMPROBAR en if\n", intposX, orientacion);
         if(intposX == 9){
             printf("ERROR, Barco sobrepasa el borde derecho del tablero\n");
             exit(0);
@@ -128,11 +120,8 @@ int comprobar(int largo, int intposX, int posY, char orientacion){
         }
     
     }
-    //ERROR:
-    //Barcos ensima de otros barcos
     return 0;
 }
-//------------------------------aqui empieza la wea del cucho
 char** imprimir_pantalla(char** Jpos){
     
     char** matriz = malloc(10*sizeof(char*));
@@ -147,16 +136,6 @@ char** imprimir_pantalla(char** Jpos){
             printf("[%c]",matriz[i][j]);
         }
     } 
-    
-
-    /*
-    //Archivo be like:
-    //Jpos[x][0]; Largo barco
-    //Jpos[x][1]; Posicion en x (A, B, C ...)
-    //Jpos[x][2]; Posicion en y (0, 1, 2, ...)
-    //Jpos[x][3]; //orientacion
-    */
-    
     int largo;
     char posX;
     int posY;
@@ -166,26 +145,14 @@ char** imprimir_pantalla(char** Jpos){
     for (int c=0;c<4;c++){ //Recorrer Filas del array
         //Largo barco
         largo = Jpos[c][0] - 48;
-        //printf("%d largo\n", largo);
-
         //PosX
-        posX = Jpos[c][1];
-        //printf("%c Jpos despues\n", Jpos[c][1]);
-        printf("%c posX\n", posX);  
-
-
+        posX = Jpos[c][1]; 
+        //Transformar a int
         intposX = traducir(posX);
-
-
-        //printf("%d int posX\n", intposX);
         //PosY
         posY = Jpos[c][2] - 48;
-        //printf("%d posY\n", posY);
         //oreintacion 
         orientacion = Jpos[c][3];
-        //printf("%c orientacion\n", (char)orientacion); // Recorrer Las lineas y poner en MATRIZ
-        
-        //
         comprobar(largo, intposX, posY, orientacion); //C O M P R O B A R  
         if (orientacion == 104){ //horizontal
             if(matriz[posY][intposX] > 96 && matriz[posY][intposX] < 101){ //Comprobar superposicion
@@ -194,10 +161,8 @@ char** imprimir_pantalla(char** Jpos){
             }
             for(int i=0;i<largo;i++){
                 matriz[posY][intposX] = lista[c];
-                //printf("%d , %d horizontal \n", intposX, posY);
                 intposX ++;
                 }
-            
         }
         else if(orientacion == 118){ //Vertical
             if(matriz[posY][intposX] > 96 && matriz[posY][intposX] < 101){ 
@@ -206,57 +171,50 @@ char** imprimir_pantalla(char** Jpos){
             }
             for(int i=0;i<largo;i++){
                     matriz[posY][intposX] = lista[c];
-                    //printf("%d , %d vertical \n", intposX, posY);
                     posY++;
                 }
         }
-        printf("\n");//yap hacer hitscan aqui;
-         
-    }
-    
-
-    printf("  A  B  C  D  E  F  G  H  I  J\n");
-
+        printf("\n");
+    } 
+    //Tablero
+    //printf("  A  B  C  D  E  F  G  H  I  J\n");
      for (int i=0;i<10;i++){
-         printf("%d",i);
+         //printf("%d",i);
         for (int j=0 ; j<10;j++){
             char pos_actual = matriz[i][j]; 
             if (pos_actual == 97){//a
                 matriz[i][j] = lista[0];
-                printf("[%c]",matriz[i][j]);
+                //printf("[%c]",matriz[i][j]);
                 continue;
             }
             if (pos_actual == 98){//b
                 matriz[i][j] = lista[1];
-                printf("[%c]",matriz[i][j]);
+                //printf("[%c]",matriz[i][j]);
                 continue;
             }
             if (pos_actual == 99){//c
                 matriz[i][j] = lista[2];
-                printf("[%c]",matriz[i][j]);
+                //printf("[%c]",matriz[i][j]);
                 continue;
             }
             if (pos_actual == 100){//d
                 matriz[i][j] = lista[3];
-                printf("[%c]",matriz[i][j]);
+                //printf("[%c]",matriz[i][j]);
                 continue;
             }  
             else{
                 matriz[i][j] = '_';
             }
-             ; //rellear con '_' ya que no hay barco
-            printf("[%c]",matriz[i][j]);
+            //printf("[%c]",matriz[i][j]);
             }
-        printf("%d\n",i);
+        //printf("%d\n",i);
         if (i==9){
-            printf("  A  B  C  D  E  F  G  H  I  J\n");
+            //printf("  A  B  C  D  E  F  G  H  I  J\n");
             }
         }
 
     return matriz; //matriz es el juego 
 }
-//aqui termina la wea del cucho---------------------------------|
-
 char* concat(const char *s1, const char *s2){
     char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
     // in real code you would check for errors in malloc here
@@ -264,9 +222,7 @@ char* concat(const char *s1, const char *s2){
     strcat(result, s2);
     return result;
     }
-
 //recibe coordenada del bot-----------------------------------------------|
-//hacer que no coloque dos veces la msima coordenada
 char** atacar_bot(char** matriz){
     int columna;
     int fila;
@@ -280,6 +236,7 @@ char** atacar_bot(char** matriz){
             printf("Impacto!\n");
             break;
         }
+        //Comprobacion de misma coordenada
         else if((matriz[fila][columna] == 46) //Comprobar Agua 
                 || (matriz[fila][columna] == 88)){ //Comprobar X
                 printf("Esas coordenadas ya fueron Seleccionadas! \n");
@@ -295,7 +252,6 @@ char** atacar_bot(char** matriz){
 
 }
 //recibe coordenada del jugador-----------------------------------------------|
-//hacer que no coloque dos veces la msima coordenada
 char** atacar(char** matriz){
     char cordenada_col[5];//scanf es mas retrasado q un monooooooo,segun yo para los int?
     int coordenada_fila;
@@ -339,7 +295,8 @@ char** atacar(char** matriz){
             printf("Impacto!\n");
             break;
             
-        }//Error: colocar la coordenada ya puesta
+        }
+        //Comprobacion de misma coordenada
         else if((matriz[coordenada_fila][columna] == 46) //Comprobar Agua 
             || (matriz[coordenada_fila][columna] == 88)){ //Comprobar X
             printf("Esas coordenadas ya fueron Seleccionadas! \n");
@@ -352,97 +309,52 @@ char** atacar(char** matriz){
             break;
         }
     }
-        //  matriz[columna][coordenada_fila] = ;
-        //sprintf(s_fila,"%d",coordenada_fila);
-        // print our string
         printf("coordenadas ingresadas: %d %d\n",coordenada_fila, columna);
-        return matriz;//le tengo q poner &?
-
+        return matriz;
     }
 
-
-
-
-
-
 char** leer(FILE* carpeta){
-    
-
     char* token;
     int i;
     char **lineas = malloc(sizeof(char*)*4);
     
     if (carpeta != NULL){
         char line[255];
-        
         int contador = 0;
 
-        
-
-        
         while ((fgets(line, sizeof line, carpeta)) != NULL){
             char* linea = malloc(sizeof(char)*4);
             token = strtok(line, ";");
-            
-            //strcpy(linea,"");
-            
-            for(i=0;i<5;i++){
-                
-                
-                
-                if(i==0){   
 
-                    //printf("\n%s\n",token);
+            for(i=0;i<5;i++){
+
+                if(i==0){   
                     token = strtok(NULL,";");
                     }
                 
                 if(i==1){  
-
                     linea=concat(linea,token);
-                    //printf("%d\n",atoi(token));
                     token = strtok(NULL,";");
-                    
                     }
                     
                 if(i==2){   
                     linea=concat(linea,token);
-                    //printf("%s\n",token);
                     token = strtok(NULL,";");
-                    
                     }
                     
                 if(i==3){   
                     linea=concat(linea,token);
-                    //printf("%d\n",atoi(token));
                     token = strtok(NULL,";");
                     }
 
                 if(i==4){   
                     linea=concat(linea,token);
-                    //printf("%s\n",token);
                     token = strtok(NULL,"\n");
                     }
-                
-                
             }
             lineas[contador] = linea;
-
             contador++;
-        
-            
-            
         }
-
-        
-        for (int i=0;i<4;i++){
-            for(int j =0;j<4;j++){
-                //printf("\n%c",lineas[i][j]);
-            }
-        }
-        
-        
-        //fclose(carpeta);
-
     }
     else{
         printf("Error\n");  
@@ -452,11 +364,7 @@ char** leer(FILE* carpeta){
     
 }
 
-
-//gcc -std=c99 -Wall -Wextra -Wundef -Werror -Wuninitialized -Winit-self Delrio-Ballivian-Tarea1.c -o salida  && ./salida -v posicionesJ1 posicionesJ2
-
 int main (int argc, char** argv){
-//int main ()
     //Modo
     //char *mode = argv[1];
     printf("%d",argc);
@@ -468,9 +376,6 @@ int main (int argc, char** argv){
 
     char* Archivo2=concat(file_J2,".txt");
     char* Archivo1=concat(file_J1,".txt");
-    printf("%s\n",Archivo2);
-    printf("%s\n",Archivo1);
-
 
     FILE*pos_J1;
     FILE*pos_J2;
@@ -496,7 +401,7 @@ int main (int argc, char** argv){
                 printf("\nturno player_1\n");
                 imprimir_tablero(tablero_2);
                 tablero_2 = atacar(tablero_2);
-                imprimir_tablero_Cheat(tablero_2);
+                //imprimir_tablero_Cheat(tablero_2); DEBUG
                 printf("cantes de entrar a condiciones");
                 condicion_ganar(tablero_2,player_num);
                 player_num = 1;
@@ -508,16 +413,11 @@ int main (int argc, char** argv){
                 printf("turno player_2");
                 imprimir_tablero(tablero_1);
                 tablero_1 = atacar(tablero_1);
-                imprimir_tablero_Cheat(tablero_1);
+                //imprimir_tablero_Cheat(tablero_1); DEBUG
                 condicion_ganar(tablero_1,player_num);
                 player_num = 2; 
             }
- 
-
         }
-
-            //algo asi planeo y
-        //free(matriz);
     }
     //MODO AUTO 
     else if((strcmp(argv[1], "-a")) == 0){
@@ -527,15 +427,15 @@ int main (int argc, char** argv){
             imprimir_tablero(tablero_2);
             tablero_2 = atacar(tablero_2);
             imprimir_tablero(tablero_2);
-            //imprimir_tablero_Cheat(tablero_1);
+            //imprimir_tablero_Cheat(tablero_1); DEBUG
             printf("cantes de entrar a condiciones");
             condicion_ganar(tablero_2,player_num);
-            //turno del bot qlo
+            //turno del bot 
             printf("\nes turno del CPU\n");
             imprimir_tablero(tablero_1);
             tablero_1 = atacar_bot(tablero_1);
             imprimir_tablero(tablero_1);
-            //imprimir_tablero_Cheat(tablero_1);
+            //imprimir_tablero_Cheat(tablero_1); DEBUG
 
             condicion_ganar(tablero_1,player_num);
 
@@ -551,3 +451,10 @@ int main (int argc, char** argv){
     fclose(pos_J2);
     return 0;
 }
+//Archivo be like:
+    //Jpos[x][0]; Largo barco
+    //Jpos[x][1]; Posicion en x (A, B, C ...)
+    //Jpos[x][2]; Posicion en y (0, 1, 2, ...)
+    //Jpos[x][3]; //orientacion
+//gcc -std=c99 -Wall -Wextra -Wundef -Werror -Wuninitialized -Winit-self Delrio-Ballivian-Tarea1.c -o salida  && ./salida -v posicionesJ1 posicionesJ2
+//LIMPIAR MATRIZES QUE SE PONEN PQ SI.
